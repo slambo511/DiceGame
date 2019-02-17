59a863247186619ab6a94c5bef3d8af5bbaf0457
Samuel Bancroft <samuelbancroft@hotmail.com>
2019-02-17 01:17:25 +0000

    Tided comments to be PEP8 compliant.

--- DiceGame/DiceGame.py
+++ DiceGame/DiceGame.py
@@ -81,10 +81,11 @@ class Die:
 #	 •The points rolled on each player’s dice are added to their score.
 #    •If the total is an even number, an additional 10 points are added to their score.
 #  	 •If the total is an odd number, 5 points are subtracted from their score.
-#	 •If they roll a double, they get to roll one extra die and get the number of points rolled added to 
-#	  their score.
+#	 •If they roll a double, they get to roll one extra die and get the number of points 
+#	  rolled added to their score.
 #	 •The score of a player cannot go below 0 at any point.
 #	 •The person with the highest score at the end of the 5 rounds wins.
-#	 •If  both  players  have  the  same  score  at  the  end  of  the  5  rounds,  they  each  roll  1  die  and  
-#	  whoever gets the highest score wins (this repeats until someone wins).
+#	 •If  both  players  have  the  same  score  at  the  end  of  the  5  rounds,  they  
+#	  each  roll  1  die  and whoever gets the highest score wins (this repeats until 
+#	  someone wins).
 
