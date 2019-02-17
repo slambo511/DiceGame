96e29ee8ea74eff8f72bb71141480100e52d8934
Samuel Bancroft <samuelbancroft@hotmail.com>
2019-02-17 01:16:18 +0000

    Added rules, ready for section 3.

--- DiceGame/DiceGame.py
+++ DiceGame/DiceGame.py
@@ -77,3 +77,14 @@ class Die:
 
 # 3.Calculates and outputs the points for each round and each player’s total score.
 
+# The rules are:
+#	 •The points rolled on each player’s dice are added to their score.
+#    •If the total is an even number, an additional 10 points are added to their score.
+#  	 •If the total is an odd number, 5 points are subtracted from their score.
+#	 •If they roll a double, they get to roll one extra die and get the number of points rolled added to 
+#	  their score.
+#	 •The score of a player cannot go below 0 at any point.
+#	 •The person with the highest score at the end of the 5 rounds wins.
+#	 •If  both  players  have  the  same  score  at  the  end  of  the  5  rounds,  they  each  roll  1  die  and  
+#	  whoever gets the highest score wins (this repeats until someone wins).
+
